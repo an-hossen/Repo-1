e5ae0e747dbac5e8d4251c8158e5031aaa5f0e10
Anwar <anhossen97@gmail.com>
2024-10-24 16:12:45 +0600

    The highest frequency character in a string.

---
+++ frqncy.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+
+int main() {
+
+    char str[1000],c;
+
+    int i,j,k = 0,n = 0,index[1000], cnt, mx[1000], max = 1;
+
+    printf("Enter Your String : ");
+
+    scanf("%[^\n]s",&str);  //Input a string from user.
+
+    for(i = 0; str[i] != '\0'; i++) {  
+
+        cnt = 1;
+
+        for(j=i+1;str[j]!='\0';j++) {  //Every character checking.
+
+            if(str[i] == str[j]) {
+
+                cnt ++;  //How many times a character present.
+
+                n++;   //The times of all true conditions.
+
+                index[k] = j;  //Store every location of true conditions.
+
+                k++; 
+            }
+        }
+        
+        mx[i] = cnt; // Store the frequences.
+        
+        if(str[i]!=' '){  //Space will not count for frequency.
+            
+            printf(" %c is %d times.\n",str[i],cnt);
+        }
+
+        for(int x = 0; x<n; x++){
+
+            if(i+1 == index[x]){  //Avoiding those location where it's same character verifyed.
+
+                i+=1;
+
+                break;
+            }
+        }
+
+    }
+    
+    for(i=0;str[i]!='\0';i++){  //Checking for the highest frequency character.
+        
+        if(mx[i]>max){
+            
+            max = mx[i];
+            
+            c = str[i];
+        }
+    }
+    
+    printf("\nSo %c has the highest frequency.\n",c);
+    
+    return 0;
+}
